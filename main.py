12214a04d4c59327eef6e884868b667edde8386f
Paweł <mielczarekpawel26@gmail.com>
2024-12-12 17:00:13 +0100

    exit and feedback added

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ screen.setup(800, 600)
 image = "blank_states_img.gif"
 screen.addshape(image)
 t.shape(image)
-points = 49
 writer = t.Turtle()
 writer.hideturtle()
 writer.penup()
 data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
 guessed_states = []
-while points < 50:
-    answer = screen.textinput(f"{points}/{len(data.state)} correct states","Gues another state").title()
-    for state in data.state:
-        if state == answer:
-            if answer not in guessed_states:
-                new_x = int(data[data.state == answer].x.iloc[0])
-                new_y = int(data[data.state == answer].y.iloc[0])
-                writer.goto(new_x,new_y)
-                writer.write(answer)
-                guessed_states.append(answer)
-                points += 1
-screen.exitonclick()
+while len(guessed_states) < 50:
+    answer = screen.textinput(f"{len(guessed_states)}/50 correct states","Gues another state").title()
+    if answer == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(missing_states)
+        break
+    if answer in all_states:
+        states_data = data[data.state == answer]
+        writer.goto(states_data.x.item(), states_data.y.item()) # I have used int(states_data.x) it worked too
+        writer.write(answer)
+        guessed_states.append(answer)
+
